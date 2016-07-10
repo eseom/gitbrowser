e4795f4661e6cc7f967fe21c9f8ab5fddfc0cf65
EunseokEom <me@eseom.org>
2016-07-10 23:56:44 +0900

    add profile controller

--- flamengo/static/web/src/js/controllers/profile.js
+++ flamengo/static/web/src/js/controllers/profile.js
@@ -3,9 +3,31 @@ define([], function () {
   return {
     url: '/profile',
     resolve: {
+      profile: function ($http) {
+        return $http.get('/auth/me');
+      }
     },
-    controller: function profileCtrl($scope, $rootScope, $stateParams) {
+    controller: function profileCtrl($scope, $rootScope, $http, profile) {
       $rootScope.title = 'profile';
+      $scope.user = profile.data.user;
+
+      $scope.save = function () {
+        var data = {
+          id: $scope.user.id,
+          nickname: $scope.user.nickname,
+          name: $scope.user.name
+        };
+        if ($scope.user.password !== '' && $scope.user.password === $scope.user.passwordConfirm)
+          data.password = $scope.user.password;
+        $http({
+          method: 'POST',
+          url: '/auth/save',
+          data: data
+        }).then(function (response) {
+          $scope.user.password = '';
+          $scope.user.passwordConfirm = '';
+        });
+      }
     }
   };
 });
